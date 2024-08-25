00ef35e0b722bde112d14b7989ef05b288d0f507
Aliwoto <aminnimaj@gmail.com>
2024-08-25 20:55:03 +0330

    Fix SQL syntax error in EditCourseInfo.
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- src/database/helpers_courses.go
+++ src/database/helpers_courses.go
@@ -44,15 +44,11 @@ func CreateNewCourse(data *NewCourseData) (*CourseInfo, error) {
 func EditCourseInfo(data *EditCourseInfoData) (*CourseInfo, error) {
 	_, err := DefaultContainer.db.Exec(context.Background(),
 		`
-		BEGIN;
-			-- Begin the transaction.
-			UPDATE course_info
+		UPDATE course_info
 			SET course_name = $1,
 				course_description = $2,
 				topic_id = $3
 			WHERE course_id = $4;
-			
-		COMMIT;
 		`,
 		data.CourseName,
 		data.CourseDescription,
