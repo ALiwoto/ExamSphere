84824960f26ce8e54343dbb3db1502a8011619ea
Aliwoto <aminnimaj@gmail.com>
2025-02-03 15:47:03 +0330

    Minor fixes to CreateExamData.IsValid method for sample exams.
    Signed-off-by: Aliwoto <aminnimaj@gmail.com>

--- src/apiHandlers/examHandlers/methods.go
+++ src/apiHandlers/examHandlers/methods.go
@@ -1,10 +1,19 @@
 package examHandlers
 
-import "time"
+import (
+	"time"
+)
 
 func (d *CreateExamData) IsValid() bool {
+	if d.IsSampleExam {
+		d.ExamDate = 0
+		d.NeedsVideoCall = false
+		d.NeedsVoiceCall = false
+		d.Duration = 0
+		return true
+	}
+
 	return d.CourseId != 0 &&
-		d.Price != "" &&
 		d.Duration > 0 &&
 		d.ExamDate >= time.Now().UTC().Unix()
 }
